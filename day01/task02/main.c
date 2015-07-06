21685914fbbe576ded255c0376652e080952ecaa
Test <test@example.com>
2015-07-06 15:46:04 +0200

    day01: task02 2 threads

--- day01/task02/main.c
+++ day01/task02/main.c
@@ -3,22 +3,50 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
+static int num = 0;
 
-void *f1(void *a)
+bool p = true;
+bool end = false;
+
+void ping(void *a)
 {
-	pthread_mutex_lock(&mutex);
+	do
+	{
+		pthread_mutex_lock(&mutex);
+		if (p)
+		{
+			printf("Ping\n");
+			p=false;
+		}
+		pthread_mutex_unlock(&mutex);
+		//pthread_yield();
+	}
+	while (!end);
 
-	pthread_cond_wait(&cond, &mutex);
+}
 
-	printf("Woked up!\n");
-
-	pthread_mutex_unlock(&mutex);
-
-	return 0;
+void pong(void *a)
+{
+	do
+	{
+		pthread_mutex_lock(&mutex);
+		if (!p)
+		{
+			printf("\tPong\n");
+			p=true;
+			--num;
+			if (!num)
+				end = true;
+		}
+		pthread_mutex_unlock(&mutex);
+		//pthread_yield();
+	}
+	while (!end);
 }
 
 int main(int argc, char **argv)
@@ -29,17 +57,15 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	pthread_t tid;
+	num = atoi(argv[1]);
 
-	pthread_create(&tid, NULL, (void*)f1, &cond);
+	pthread_t tid[2];
 
-	sleep(1);
-	pthread_mutex_lock(&mutex);
-	printf("Wake up!\n");
-	pthread_cond_signal(&cond);
-	pthread_mutex_unlock(&mutex);
+	pthread_create(&tid[0], NULL, (void*)ping, &cond);
+	pthread_create(&tid[1], NULL, (void*)pong, &cond);
 
-	pthread_join(tid, NULL);
+	pthread_join(tid[0], NULL);
+	pthread_join(tid[1], NULL);
 
 
 
